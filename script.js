91745a31ce3eabd29a175d9a427739caed1da7db
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-01 12:24:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,8 @@ let userData = {
 
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
-
+  audio.src = song.src;
+  audio.title = song.title;
 };
 
 const renderSongs = (array) => {
