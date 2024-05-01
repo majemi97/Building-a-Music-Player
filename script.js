1aca9f6b1505a4c6f544b5a18df0e0a66ae40ff7
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-01 11:58:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -107,13 +107,15 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
-
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
-      return -1
+      return -1;
     }
-  });
-};
 
+  if (a.title > b.title) {
+    return 1;
+  }
+  });
+}
 renderSongs(userData?.songs);
