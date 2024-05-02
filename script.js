99141deb5a47c6c8f11e6bbb8320331e58e082ed
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 13:01:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -163,7 +163,8 @@ const deleteSong = (id) => {
 
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
-  
+  resetButton.appendChild(resetText);
+  playlistSongs.appendChild(resetButton);
   }
 
 };
