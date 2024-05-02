53374b28ea8b58e7c6634098923df4f0bca819b5
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 13:06:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -166,9 +166,11 @@ const deleteSong = (id) => {
     resetButton.appendChild(resetText);
     playlistSongs.appendChild(resetButton);
 
-  resetButton.addEventListener("click", () => {
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
 
-  });
+    });
+
   }
 
 };
