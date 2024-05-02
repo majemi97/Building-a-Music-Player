eb7d6a4dc381c3ea758c89d4deaac83799f4822c
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 12:13:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  playSong(setPlayerDisplay());
   audio.play();
 };
 
@@ -135,8 +136,9 @@ const setPlayerDisplay = () => {
   const songArtist = document.getElementById("player-song-artist");
   const currentTitle = userData?.currentSong?.title;
   const currentArtist = userData?.currentSong?.artist;
+
   playingSong.textContent = currentTitle ? currentTitle : "";
-songArtist.textContent = currentArtist ? currentArtist : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
