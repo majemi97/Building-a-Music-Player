31e15c2b0d514dc27b943dc65922a6b53629ad8e
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 11:09:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,12 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-  
+  if (userData?.currentSong === null) {
+    return;
+  }
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+  }
 };
 
 const renderSongs = (array) => {
