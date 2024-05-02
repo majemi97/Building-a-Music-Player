5a9b8eefbaa11ab146a39817c8ef083c65133462
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 11:38:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -134,10 +134,10 @@ const highlightCurrentSong = () => {
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
-  
-playlistSongElements.forEach((songEl) => {
 
-});
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
 };
 
 const renderSongs = (array) => {
