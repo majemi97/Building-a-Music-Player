ff33f725c501509b5272610fbc676fa2bb60da29
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 11:50:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -131,8 +131,9 @@ const playPreviousSong = () => {
 };
 
 const setPlayerDisplay = () => {
-  
-}
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+};
 
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
