7a86bc0771d142c479065f3cbe864910f640c1b0
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-02 10:52:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const renderSongs = (array) => {
 };
 
 const getCurrentSongIndex = () => {
-
+  return userData?.songs.indexOf(userData?.currentSong);
 }
 
 playButton.addEventListener("click", () => {
